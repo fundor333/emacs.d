13e7158818e4d80bdad5a06629714227d653d152
Fundor333 <fundor333@gmail.com>
2017-04-19 22:18:17 +0200

    Adding my basic EMACS config

---
+++ init.el
@@ -0,0 +1,9 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
