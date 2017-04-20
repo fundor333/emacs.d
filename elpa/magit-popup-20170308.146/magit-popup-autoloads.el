3a23730d28030b19d807bd5bf8b0d1001810935d
Fundor333 <fundor333@gmail.com>
2017-04-20 09:51:20 +0200

    Adding Magit to the configuration

---
+++ elpa/magit-popup-20170308.146/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22776 26359 782914 771000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
