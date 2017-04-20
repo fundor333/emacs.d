65ebd80321a647fdd0a164a816fd9c8bd539888d
Fundor333 <fundor333@gmail.com>
2017-04-20 12:26:23 +0200

    Adding packages support
    Adding a packaging support for upgrading and updating the packages

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+(load "~/.emacs.d/init-packages")
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
