a29eefed03008ba1651f205b2852e0ea4f0f1223
Fundor333 <fundor333@gmail.com>
2017-04-20 09:55:14 +0200

    Adding the setting for the gitignore

--- elpa/magit-gitflow-20160809.1335/magit-gitflow-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; magit-gitflow-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "magit-gitflow" "magit-gitflow.el" (22776 26480
-;;;;;;  628701 34000))
-;;; Generated autoloads from magit-gitflow.el
-
-(autoload 'turn-on-magit-gitflow "magit-gitflow" "\
-Unconditionally turn on `magit-gitflow-mode'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("magit-gitflow-pkg.el") (22776 26480 639060
-;;;;;;  806000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-gitflow-autoloads.el ends here
