9723639f791f703fe4c0753d96fec6fbc6c72296
Fundor333 <fundor333@gmail.com>
2017-05-31 14:14:51 +0200

    Fix theme bug

--- init.el
+++ init.el
@@ -1,26 +1,20 @@
-
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (require 'org)
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-
-
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
+(load-theme 'dracula t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" default)))
- '(safe-local-variable-values (quote ((docker-image-name . "fundor333/hugo")))))
+    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'upcase-region 'disabled nil)
