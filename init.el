106612fce2f950f4d2dc49b35a0c04a1642255b0
Fundor333 <fundor333@gmail.com>
2017-08-26 20:22:48 +0200

    Updating the configuration, fixing some bug

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 (load-theme 'dracula t)
@@ -12,6 +19,9 @@
  '(custom-safe-themes
    (quote
     ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "b97a01622103266c1a26a032567e02d920b2c697ff69d40b7d9956821ab666cc" default)))
+ '(package-selected-packages
+   (quote
+    (pelican-mode web-mode scss-mode powerline php-mode pdf-tools org-bullets neotree markdown-mode less-css-mode jedi helm-projectile go-mode go-autocomplete emojify editorconfig dracula-theme dockerfile-mode all-the-icons achievements)))
  '(safe-local-variable-values (quote ((docker-image-name . "fundor333/hugo")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
